a23f56898659166dbe1b5fded1ecc4728207a425
Teeraphat Kullanankanjana <watit2013@gmail.com>
2024-01-28 13:23:16 +0700

    Sets the color(s) of the LEDs

---
+++ example/cyberpi/01-RGB LED/01-set_color.py
@@ -0,0 +1,8 @@
+import cyberpi
+
+#               R  G B Position
+cyberpi.led.on(255,0,0,id=1)
+cyberpi.led.on(255,255,0,id=2)
+cyberpi.led.on(255,255,255,id=3)
+cyberpi.led.on(0,255,0,id=4)
+cyberpi.led.on(0,255,255,id=5)
