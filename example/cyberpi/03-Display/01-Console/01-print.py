4ef16d432fa22418ba1699e27727349f1345fcef
Teeraphat Kullanankanjana <watit2013@gmail.com>
2024-10-13 22:10:57 +0700

    Print the message
    Print the message

--- example/cyberpi/03-Display/01-Console/01-print.py
+++ example/cyberpi/03-Display/01-Console/01-print.py
@@ -1,5 +1,8 @@
 import cyberpi
 
+# font_size = 12,16(default),24,32
+cyberpi.console.set_font(16)
+
 cyberpi.console.println("NewLine ")
 cyberpi.console.print("Not")
 cyberpi.console.print(" Newline")
