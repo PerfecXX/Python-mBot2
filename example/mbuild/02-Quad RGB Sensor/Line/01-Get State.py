671b74116a549ec5824ddc8b617a28a41f34bb8f
Teeraphat Kullanankanjana <watit2013@gmail.com>
2024-11-24 14:35:07 +0700

    Get Line and BG State (0-15)
    Get Line and BG State (0-15)

--- example/mbuild/02-Quad RGB Sensor/Line/01-Get State.py
+++ example/mbuild/02-Quad RGB Sensor/Line/01-Get State.py
@@ -4,8 +4,8 @@ cyberpi.display.show_label("LINE:\nBG:", 16, 0, 0, index = 0)
 
 while True:
   
-    line_state = mbuild.quad_rgb_sensor.get_line_sta()
-    bg_state = mbuild.quad_rgb_sensor.get_ground_sta()
+    line_state = mbuild.quad_rgb_sensor.get_line_sta("all", 1)
+    bg_state = mbuild.quad_rgb_sensor.get_ground_sta("all", 1)
   
     cyberpi.display.show_label(line_state, 16, 50, 0, index = 1)
     cyberpi.display.show_label(bg_state, 16, 50, 15, index = 2)
