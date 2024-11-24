24f89d7f48db3d83eace9c03c426a96ba5b7ed89
Teeraphat Kullanankanjana <watit2013@gmail.com>
2024-11-24 15:52:46 +0700

    Simple Image Animation
    Simple Image Animation

---
+++ example/mbuild/04-8x16 Blue LED Matrix/04-Image Animation.py
@@ -0,0 +1,6 @@
+import cyberpi, mbuild
+
+mbuild.led_panel.show_image("00003c7e7e3c000000003c7e7e3c0000", time_s = 1, index = 1)
+mbuild.led_panel.show_image("00103030303010000010303030301000", time_s = 1, index = 1)
+mbuild.led_panel.show_image("0000181c1c1800000000181c1c180000", time_s = 1, index = 1)
+mbuild.led_panel.show_image("00003c7e7e3c000000003c7e7e3c0000", time_s = 1, index = 1)
