d3dad57f896e897d850a55cf236f573df91608ca
Teeraphat Kullanankanjana <watit2013@gmail.com>
2025-06-20 02:18:47 +0700

    Get touch sensor state
    Get touch sensor state

---
+++ example/mbuild/08-Science Sensor/04-Touch Sensor/01-get_touch.py
@@ -0,0 +1,9 @@
+import cyberpi, mbuild
+
+while True:
+  
+      if mbuild.science.touch_is_active(1):
+        cyberpi.led.on(208, 2, 27, "all")
+        
+      else:
+        cyberpi.led.off("all")
