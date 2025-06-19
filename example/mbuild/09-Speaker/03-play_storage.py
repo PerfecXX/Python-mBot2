adfcb82e6c26127b85d8642b382b1eb219e5b9d0
Teeraphat Kullanankanjana <watit2013@gmail.com>
2025-06-20 03:43:54 +0700

    Play sound from speaker storage
    Play sound from speaker storage

--- example/mbuild/09-Speaker/03-play_storage.py
+++ example/mbuild/09-Speaker/03-play_storage.py
@@ -6,3 +6,5 @@ mbuild.speaker.set_volume(100, 1)
 mbuild.speaker.play_melody("0001", 1)
 # your file name in speaker storage must be 4 digit
 mbuild.speaker.play_melody_until_done('0002', 1)
+
+mbuild.speaker.stop_sounds(1)
