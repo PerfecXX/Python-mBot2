2fede190ac3d2562186a8162b4e3266bd6484816
Teeraphat Kullanankanjana <watit2013@gmail.com>
2025-06-20 17:48:35 +0700

    Get light level from sensor
    Get light level from sensor

---
+++ example/mbuild/12-Dual RGB Sensor/01-get_light.py
@@ -0,0 +1,9 @@
+import cyberpi, mbuild
+
+while True:
+  
+  probe1_light = mbuild.dual_rgb_sensor.get_intensity('RGB1', 1)
+  probe2_light = mbuild.dual_rgb_sensor.get_intensity('RGB2', 1)
+  
+  cyberpi.display.show_label(probe1_light, 12, 0, 0, index = 0)
+  cyberpi.display.show_label(probe2_light, 12, 0, 16, index = 1)
